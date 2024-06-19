e77ff7664fda2ad646991e2d95698bbe26514b78
victorliu1231 <43052540+victorliu1231@users.noreply.github.com>
2024-06-19 11:35:45 -0400

    test

--- Assets/Scripts/Generators/EnemySpawner.cs
+++ Assets/Scripts/Generators/EnemySpawner.cs
@@ -22,7 +22,6 @@ public class EnemySpawner : MonoBehaviour
         yield return new WaitForSeconds(spawnTime * GameManager.Instance.stage);
         GameObject enemy = Instantiate(go, transform.position, Quaternion.identity);
 
-        GameObject parent = GameObject.FindWithTag("EnemyParentTransform");
-        enemy.transform.parent = parent.transform;
+        enemy.transform.parent = GameManager.Instance.enemiesParent;
     }
 }
